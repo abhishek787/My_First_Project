d084745e706741ed20b44abdce71744f76fa0ed0
abhishek787 <asabhishek787@gmail.com>
2017-08-13 20:31:52 +0530

    git configuration file added

---
+++ spychat/main.py
@@ -0,0 +1,7 @@
+print 'let\'s get started....'
+spy_name=raw_input('what is your name')
+if len(spy_name) > 0:
+    spy_salutation = raw_input('what shoud we call you')
+    print 'welcome ' + spy_name + ' we are happy you are alive'
+else:
+    print 'you are so great'
